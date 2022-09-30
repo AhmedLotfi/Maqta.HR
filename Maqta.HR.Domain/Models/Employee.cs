513a3369faa665c96b2655b67a838a2e85f900e3
ahmedlotfiali <ahmedlotfiali@gmail.com>
2022-09-30 21:01:11 +0400

    adding Employee model

---
+++ Maqta.HR.Domain/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Maqta.HR.Domain.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string FirstName { get; set; } = default!;
+
+        [Required]
+        [MaxLength(150)]
+        public string LastName { get; set; } = default!;
+
+        [Required]
+        [MaxLength(100)]
+        public string Email { get; set; } = default!;
+
+        [Required]
+        [MaxLength(300)]
+        public string Password { get; set; } = default!;
+
+        [NotMapped]
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
